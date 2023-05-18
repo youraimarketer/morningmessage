2b84ac3e7fe844a371de85da2a5ad97d4480ab7d
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-17 23:47:54 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -90,12 +90,12 @@ const Home: NextPage = () => {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <a
           className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
-          href="https://github.com/youraimarketer/morningmessage"
+          href="https://twitter.com/Jeezaicareer"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Github />
-          <p>Star on GitHub</p>
+          <p>Follow on Twitter</p>
         </a>
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
           Generate your next cover letter with ChatGPT-4!
