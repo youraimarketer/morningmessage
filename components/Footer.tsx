32385278f89588123754e578dce10631b67f02eb
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-17 21:24:14 -0400

    s

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -25,7 +25,7 @@ export default function Footer() {
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
-          href="https://twitter.com/nutlope"
+          href="https://twitter.com/youraimarketer"
           className="group"
           aria-label="TaxPal on Twitter"
         >
