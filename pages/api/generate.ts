7540cece592a2f61006509ece1b4284e4ca59ec5
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-18 00:49:02 -0400

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -20,11 +20,11 @@ const handler = async (req: Request): Promise<Response> => {
   const payload: OpenAIStreamPayload = {
     model: "gpt-3.5-turbo",
     messages: [{ role: "user", content: prompt }],
-    temperature: 0.4,
+    temperature: 0.7,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0,
-    max_tokens: 300,
+    max_tokens: 200,
     stream: true,
     n: 1,
   };
