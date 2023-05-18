1b852b5de2d22f632e32fe9c0034744f20e67b67
youraimarketer <132029956+youraimarketer@users.noreply.github.com>
2023-05-18 00:33:45 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -130,7 +130,7 @@ setGeneratedBios((prev) => prev + chunkValue);
             }
           />
           <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
+           
             <p className="text-left font-medium">Are you a new-grad, co-op or student? </p>
           </div>
           <div className="block">
